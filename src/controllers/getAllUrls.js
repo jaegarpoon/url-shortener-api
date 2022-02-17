b5ced654cf572b599d464376389754ef4c1e422d
jaegarpoon <73332327+jaegarpoon@users.noreply.github.com>
2022-02-18 02:38:07 +0800

    Edit return json object of method getAllUrls

--- src/controllers/getAllUrls.js
+++ src/controllers/getAllUrls.js
@@ -2,8 +2,15 @@ import pool from "../db/db.js";
 
 const getAllUrls = async (req, res) => {
   try {
-    pool.query("SELECT * FROM url_table;").then((allUrls) => {
-      res.status(200).json(allUrls.rows);
+    const baseUrl = req.protocol + "://" + req.get("host");
+    pool.query("SELECT * FROM url_table;").then((urlTable) => {
+      const allUrls = urlTable.rows.map((element) => {
+        return {
+          actualUrl: element.actual_url,
+          shortenedUrl: baseUrl + "/" + element.shortened_hash,
+        };
+      });
+      res.status(200).json(allUrls);
     });
   } catch (err) {
     console.error(err);
